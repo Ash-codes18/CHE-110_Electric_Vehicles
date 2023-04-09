3ba2be104d04e9c5b190954719752156b631e467
Ashmit Mehta <amehta.ashmit2@gmail.com>
2023-04-09 10:52:34 +0530

    -Added Transparency to the Header upon Scrolling

--- script.js
+++ script.js
@@ -21,3 +21,15 @@ function scrollvm() {
     const section = document.getElementById("section3");
     section.scrollIntoView({behavior: "smooth"});
 }
+
+window.onscroll = function() {
+  var header = document.querySelector('header');
+  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  if (scrollTop > 0) {
+    header.classList.add('transparent');
+    header.style.opacity = 0.7;
+  } else {
+    header.classList.remove('transparent');
+    header.style.opacity = 1;
+  }
+}
